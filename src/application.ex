a7e5a817c6f2fdceb41db2c2acd2b6142e3b46e7
Adam Faur <adam.faur@gmail.com>
2018-09-16 00:30:23 -0700

    Add ability to configure scheme
    Add base router to stay dry
    
    Add match handler for main router
    
    Update api code for pretty json
    
    Add base test case to stay dry
    
    Version bump

--- src/application.ex
+++ src/application.ex
@@ -4,19 +4,20 @@ defmodule HttpApp.Application do
   require Logger
 
   def start(_type, _args) do
-    port = Application.get_env(:app, :port)
+    plug_scheme  = Application.get_env :app, :plug_scheme,  :http
+    plug_options = Application.get_env :app, :plug_options, [ port: 8085 ]
 
     children = [
       Plug.Adapters.Cowboy2.child_spec(
-        scheme: :http,
-        plug: HttpApp.Router,
-        options: [ port: port ]
+        scheme:  plug_scheme,
+        plug:    HttpApp.MainRouter,
+        options: plug_options
       )
     ]
 
     opts = [ strategy: :one_for_one, name: HttpApp.Supervisor ]
 
-    Logger.info("Application running on port: #{port}")
-    Supervisor.start_link(children, opts)
+    Logger.info "Application running on port: #{plug_options[:port]}"
+    Supervisor.start_link children, opts
   end
 end
