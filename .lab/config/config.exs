a3e4d9e3da835b8204603d2b6942e58d6193fab5
Adam Faur <adam.faur@gmail.com>
2018-09-16 00:30:23 -0700

    Add ability to configure scheme
    Add base router to stay dry
    
    Add match handler for main router
    
    Update api code for pretty json
    
    Add base test case to stay dry
    
    Version bump

--- .lab/config/config.exs
+++ .lab/config/config.exs
@@ -1,6 +1,17 @@
 use Mix.Config
 
-alias HttpApp.Router, as: Router
+scheme = String.to_atom(System.get_env("SERVER_PROTOCOL") || "http")
+port   = String.to_integer(System.get_env("SERVER_PORT") || "8080")
 
-config :app, port: (System.get_env("SERVER_PORT") || "8080") |> String.to_integer
-config :app, Router, server: true
+if (scheme == :https) do
+  config :app, plug_options: [
+    password: "cowboy",
+    keyfile:  Path.expand("../../conf/ssl/server.key", __DIR__),
+    certfile: Path.expand("../../conf/ssl/server.cer", __DIR__),
+    port:     port,
+  ]
+else
+  config :app, plug_options: [ port: port ]
+end
+
+config :app, plug_scheme: scheme
